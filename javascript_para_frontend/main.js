c46e4517fbb858b453537415d5d876642ebef2b7
guiwalper <guiwalper@gmail.com>
2022-07-04 22:33:41 -0300

    template de audio

--- javascript_para_frontend/main.js
+++ javascript_para_frontend/main.js
@@ -3,6 +3,6 @@ const lista_de_teclas = document.querySelectorAll('.tecla')
 for (let i = 0; i < lista_de_teclas.length; i++) {
     lista_de_teclas[i].onclick = function () {
         var tecla = lista_de_teclas[i].classList[1]
-        document.getElementById('som_' + tecla).play()
+        document.getElementById(`som_${tecla}`).play()
     }
 }
